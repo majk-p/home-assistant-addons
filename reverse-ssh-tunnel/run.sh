9ded25a88265c5f15706829e52588179b3f0f3cd
Michał Pawlik <admin@michalp.net>
2023-01-18 23:49:26 +0100

    drop debug, add read post ssh open

--- reverse-ssh-tunnel/run.sh
+++ reverse-ssh-tunnel/run.sh
@@ -18,7 +18,6 @@ if bashio::config.exists 'private_key'; then
 
   printf "%b\n" "$(bashio::config 'private_key')" > $key_file
   chmod 600 $key_file
-  cat -vet $key_file
 
   bashio::log.info "Initializing the ssh tunnel"
   
@@ -29,7 +28,7 @@ if bashio::config.exists 'private_key'; then
     -i $key_file \
     -R 9090:homeassistant.local:8123 \
     $username@$host
-
+  read 
 elif bashio::config.exists 'password'; then
   bashio::log.info "Using password authorization"
 
@@ -41,7 +40,7 @@ elif bashio::config.exists 'password'; then
     -o UserKnownHostsFile=/dev/null \
     -R 9090:homeassistant.local:8123 \
     $username@$host
-
+  read
 else 
   bashio::log.error "Neither private key nor password provided. Exiting!"
 fi 
