a28e4d025898355df192fc551dc91b723f7a8333
Michał Pawlik <admin@michalp.net>
2023-01-19 22:26:24 +0100

    simplify plgin, use autossh

--- reverse-ssh-tunnel/run.sh
+++ reverse-ssh-tunnel/run.sh
@@ -12,38 +12,20 @@ port=$(bashio::config 'server.port')
 
 bashio::log.info "Reverse tunnel configured for $username@$host"
 
-if bashio::config.exists 'private_key'; then
+bashio::log.info "Using private key authorization"
 
-  bashio::log.info "Using private key authorization"
+printf "%b\n" "$(bashio::config 'private_key')" > $key_file
+chmod 600 $key_file
 
-  printf "%b\n" "$(bashio::config 'private_key')" > $key_file
-  chmod 600 $key_file
+bashio::log.info "Initializing the ssh tunnel"
 
-  bashio::log.info "Initializing the ssh tunnel"
-  
-  ssh -o StrictHostKeyChecking=no \
-    -o UserKnownHostsFile=/dev/null \
-    -o PubkeyAuthentication=yes \
-    -o PasswordAuthentication=no \
-    -i $key_file \
-    -R 9090:homeassistant.local:8123 \
-    -N \
-    $username@$host &
+autossh -M 2000 -o StrictHostKeyChecking=no \
+  -o UserKnownHostsFile=/dev/null \
+  -o PubkeyAuthentication=yes \
+  -o PasswordAuthentication=no \
+  -i $key_file \
+  -R 9090:homeassistant.local:8123 \
+  -N \
+  $username@$host &
 
-  bashio::log.info "Keeping connection alive forever"
-  while true; do sleep 10; done
-elif bashio::config.exists 'password'; then
-  bashio::log.info "Using password authorization"
-
-  password=`bashio::config 'password'`
-
-  bashio::log.info "Initializing the ssh tunnel"
-  
-  sshpass -p $password ssh -o StrictHostKeyChecking=no \
-    -o UserKnownHostsFile=/dev/null \
-    -R 9090:homeassistant.local:8123 \
-    $username@$host
-  read
-else 
-  bashio::log.error "Neither private key nor password provided. Exiting!"
-fi 
+while true; do sleep 10; done
